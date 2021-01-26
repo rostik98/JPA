074fa155e58eb82e2cadae2024bb011c022b7216
rostikrff98 <sheremeta967@ukr.net>
2021-01-26 16:02:49 +0200

    jpa

---
+++ src/main/java/ua/lviv/lgs/service/ProductService.java
@@ -0,0 +1,10 @@
+package ua.lviv.lgs.service;
+
+import java.util.Map;
+
+import ua.lviv.lgs.domain.Product;
+import ua.lviv.lgs.shared.AbstractCrud;
+
+public interface ProductService extends AbstractCrud<Product> {
+	public Map<Integer, Product> readAllMap();
+}
